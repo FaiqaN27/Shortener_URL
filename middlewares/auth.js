33659323e27993878fcc383aa0e9fc5982d006fd
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-09-08 18:23:09 +0500

    Add header-based authentication middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,14 @@
 const { getUser } = require('../service/auth')
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
+  // const userUid = req.cookies?.uid;
+  const userUid = req.headers["authorization"];
 
   if (!userUid) return res.redirect("/login");
-  const user = getUser(userUid);
+  const token = userUid.split("Bearer ")[1];
+  // const user = getUser(userUid);
+
+  const user = getUser(token);
 
   if (!user) return res.redirect("/login");
 
@@ -13,8 +17,11 @@ async function restrictToLoggedinUserOnly(req, res, next) {
 }
 
 async function CheckAuth(req, res, next) {
-  const userUid = req.cookies?.uid;
-  const user = getUser(userUid);
+  // const userUid = req.cookies?.uid;
+  const userUid = req.headers["authorization"];
+  const token = userUid.split("Bearer ")[1];
+  // const user = getUser(userUid);
+  const user = getUser(token);
   req.user = user;
   next();
 }
